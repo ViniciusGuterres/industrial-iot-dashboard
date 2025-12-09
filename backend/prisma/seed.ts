2a2bcd46c5a91c1f6339f31f26008888bd114754
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-12-08 22:17:42 -0300

    Update seed.ts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.machine.createMany({
+    await prisma.machines.createMany({
         data: [
             { id: 'ROBOT_ARM_01', name: 'Robot Arm 01', type: 'ROBOT_ARM', location: 'Assembly Line A' },
             { id: 'PRESSA_HIDRAULICA_02', name: 'Hydraulic Press 02', type: 'HYDRAULIC_PRESS', location: 'Stamping Area' },
