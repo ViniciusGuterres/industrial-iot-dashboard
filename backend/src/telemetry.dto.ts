a298373f0d14612416561b8d5d3b87f306ae991e
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-12-08 22:29:41 -0300

    Added class-validator

--- backend/src/telemetry.dto.ts
+++ backend/src/telemetry.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsNumber } from 'class-validator';
+
 export class CreateTelemetryDto {
+    @IsString()
     machineId: string;
+
+    @IsString()
     sensorType: string;
+
+    @IsNumber()
     value: number;
 }
