e1bb57f8674d39bd1726cc141d4d254ba6d75af4
ViniciusGuterres <viniguter@gmail.com>
2026-01-18 00:56:37 -0300

    Create infra.ts

---
+++ infra/bin/infra.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { VpcStack } from '../lib/vpc-stack';
+import { DatabaseStack } from '../lib/database-stack';
+import { AuthStack } from '../lib/auth-stack';
+import { IotStack } from '../lib/iot-stack';
+import { ProcessingStack } from '../lib/processing-stack';
+import { ApiStack } from '../lib/api-stack';
+
+const app = new cdk.App();
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+};
+
+// VPC Stack - Base networking
+const vpcStack = new VpcStack(app, 'IndustrialSentinelVpc', { env });
+
+// Database Stack - DynamoDB + SQS + RDS
+const databaseStack = new DatabaseStack(app, 'IndustrialSentinelDatabase', { 
+  env,
+  vpc: vpcStack.vpc 
+});
+databaseStack.addDependency(vpcStack);
+
+// Auth Stack - Cognito
+const authStack = new AuthStack(app, 'IndustrialSentinelAuth', { env });
+
+// IoT Stack - IoT Core + Thing + Rule
+const iotStack = new IotStack(app, 'IndustrialSentinelIot', {
+  env,
+  queue: databaseStack.ingestionQueue
+});
+iotStack.addDependency(databaseStack);
+
+// Processing Stack - Lambda (Go) triggered by SQS
+const processingStack = new ProcessingStack(app, 'IndustrialSentinelProcessing', {
+  env,
+  telemetryTable: databaseStack.telemetryTable,
+  queue: databaseStack.ingestionQueue
+});
+processingStack.addDependency(databaseStack);
+
+// API Stack - API Gateway + ECS Fargate (NestJS)
+const apiStack = new ApiStack(app, 'IndustrialSentinelApi', {
+  env,
+  vpc: vpcStack.vpc,
+  telemetryTable: databaseStack.telemetryTable,
+  database: databaseStack.rdsInstance,
+  userPool: authStack.userPool
+});
+apiStack.addDependency(vpcStack);
+apiStack.addDependency(databaseStack);
+apiStack.addDependency(authStack);
